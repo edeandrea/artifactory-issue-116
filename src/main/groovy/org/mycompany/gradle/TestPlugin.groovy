15c682a61fa9ef8517fe3d0d61b176ecaede8ec7
Eric Deandrea <Eric.Deandrea@aig.com>
2017-04-13 15:40:29 -0400

    Adding test case for https://github.com/JFrogDev/build-info/issues/117

---
+++ src/main/groovy/org/mycompany/gradle/TestPlugin.groovy
@@ -0,0 +1,53 @@
+package org.mycompany.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+final class TestPlugin implements Plugin<Project> {
+	@Override
+	void apply(Project project) {
+		project.apply plugin: 'maven-publish'
+		project.apply plugin: 'com.jfrog.artifactory'
+
+		project.configure(project) {
+			artifactory {
+				contextUrl = 'http://jcenter.bintray.com'
+
+				publish {
+					repository {
+						repoKey = 'mypublishrepo'
+						username = 'user'
+						password = 'pwd'
+					}
+
+					defaults {
+						properties = ['Deployed By': System.properties['user.name']]
+						publishBuildInfo = true
+						publishArtifacts = true
+						publishPom = true
+						publishIvy = false
+					}
+				}
+
+				resolve {
+					repository {
+						repoKey = '/'
+						maven = true
+					}
+				}
+
+				clientConfig.includeEnvVars = true
+				clientConfig.envVarsExcludePatterns = '*pwd*,*password*,*PWD*,*PASSWORD*,*secret*,*SECRET*,*key*,*KEY*,sonar.login'
+				clientConfig.info.setBuildName('some-artifact')
+				clientConfig.info.setBuildNumber(version)
+				clientConfig.info.buildUrl = 'build url'
+			}
+		}
+
+		project.afterEvaluate { proj ->
+			proj.ext {
+				propertyName = 'value'
+			}
+		}
+	}
+}
---
+++ src/test/groovy/org/mycompany/gradle/TestPluginSpec.groovy
@@ -0,0 +1,43 @@
+package org.mycompany.gradle
+
+import spock.lang.Specification
+
+import org.gradle.api.Project
+import org.gradle.testfixtures.ProjectBuilder
+
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+
+class TestPluginSpec extends Specification {
+	@Rule
+	TemporaryFolder tempFolder
+
+	def getProject(String rootProjectName, String... childProjects) {
+		def projBuilder = ProjectBuilder.builder().withProjectDir(tempFolder.root)
+		def root = projBuilder.withName(rootProjectName).build()
+
+		if (childProjects) {
+			childProjects.each { projBuilder.withName(it).withParent(root).build().projectDir.mkdirs() }
+		}
+
+		root
+	}
+
+	def runProjectAfterEvaluate(Project project) {
+		project.projectEvaluationBroadcaster.afterEvaluate project, null
+	}
+
+	def "Test for ext property"() {
+		setup:
+			def project = getProject 'project'
+
+		when:
+			project.apply plugin: TestPlugin
+			runProjectAfterEvaluate project
+
+		then:
+			project.plugins.hasPlugin 'maven-publish'
+			project.plugins.hasPlugin 'com.jfrog.artifactory'
+			project.ext.propertyName == 'value'
+	}
+}
